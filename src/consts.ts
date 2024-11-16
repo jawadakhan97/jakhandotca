f885127e556784c92e319d663369ec91706ec99a
Jawad A. Khan <jawad_khan@outlook.com>
2024-11-16 02:15:06 -0500

    Update consts.ts
    Adjusted NUM values on homepage

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Jawad A. Khan",
   EMAIL: "jawad_khan@outlook.com",
-  NUM_POSTS_ON_HOMEPAGE: 0,
-  NUM_WORKS_ON_HOMEPAGE: 0,
-  NUM_PROJECTS_ON_HOMEPAGE: 0,
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
