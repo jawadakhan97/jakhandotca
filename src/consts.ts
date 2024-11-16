b03f032de59a7764abcbfd5d92de1b9e9963b3f1
Jawad A. Khan <jawad_khan@outlook.com>
2024-11-16 02:07:15 -0500

    Update consts.ts
    Added necessary information, simplified homepage.

--- src/consts.ts
+++ src/consts.ts
@@ -2,15 +2,15 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Jawad A. Khan",
-  EMAIL: "contact@jakhan.ca",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  EMAIL: "jawad_khan@outlook.com",
+  NUM_POSTS_ON_HOMEPAGE: 0,
+  NUM_WORKS_ON_HOMEPAGE: 0,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Lightweight blog and portfolio.",
+  DESCRIPTION: "A digital portfolio.",
 };
 
 export const BLOG: Metadata = {
@@ -33,12 +33,12 @@ export const SOCIALS: Socials = [
 //    NAME: "twitter-x",
 //    HREF: "https://twitter.com/markhorn_dev",
 //  },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/jawadakhan97"
-  },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/jakhan97",
-  }
-];
+//  { 
+//    NAME: "github",
+//    HREF: "https://github.com/jawadakhan97"
+//  },
+//  { 
+//    NAME: "linkedin",
+//    HREF: "https://www.linkedin.com/in/jakhan97",
+//  }
+//];
