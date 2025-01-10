17f0eab6747363fbbd6946dc0e97cfcacb7429ef
Jawad A. Khan <jawad_khan@outlook.com>
2025-01-10 08:35:24 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Jawad A. Khan",
-  EMAIL: "jawad_khan@outlook.com",
+  EMAIL: "inbox@jakhan.ca",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
