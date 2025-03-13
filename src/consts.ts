9d9ad0d72646f7c77572c68cdaa8389479ee7b56
Jawad A. Khan <jawad_khan@outlook.com>
2025-03-13 09:31:58 -0400

    removed work page and refs, animations

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,6 @@ export const SITE: Site = {
   NAME: "Jawad A. Khan",
   EMAIL: "inbox@jakhan.ca",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
@@ -18,27 +17,7 @@ export const BLOG: Metadata = {
   DESCRIPTION: "A collection of articles.",
 };
 
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "What I have done.",
-};
-
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION: "A collection of my projects.",
 };
-
-export const SOCIALS: Socials = [
-//  { 
-//    NAME: "twitter-x",
-//    HREF: "https://twitter.com/markhorn_dev",
-//  },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/jawadakhan97"
-  },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/jakhan97",
-  }
-];
